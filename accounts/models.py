bfe373ccc844bedceab025c5ea2999b0639b445f
Adesina Joshua <tolujosh1@gmail.com>
2024-02-24 11:00:33 +0100

    feature:r4esetting the layout

--- accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.validators import UnicodeUsernameValidator
-
-
--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-
-urlpatterns = [
-     path('sign-up/', views.sign_up, name='sign-up'),
-    #path('login', views.LoginView.as_view(), name='login')
-]
