1d69c817cf2a2f23e971ee1a50d9c6f3fcce6648
Adesina Joshua <tolujosh1@gmail.com>
2024-02-27 21:27:47 +0100

    feature:integrating gpa

--- results/forms.py
+++ results/forms.py
@@ -9,6 +9,7 @@ class DeclareResultForm(ModelForm):
         widgets = {
             'select_class': forms.Select(attrs={'class': 'form-control'}),
             'select_student':  forms.Select(attrs={'class': 'form-control'}),
+            'cgpa': forms.NumberInput(attrs={'class': 'form-control'})
         }
         labels = {
             'select_class' : 'Class',
