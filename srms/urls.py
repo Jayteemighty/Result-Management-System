c6fd32e7c8e1ea408d15047f7c4dc9fd31a18531
Adesina Joshua <tolujosh1@gmail.com>
2024-03-12 12:01:07 +0100

    prep for deployment

--- srms/urls.py
+++ srms/urls.py
@@ -2,7 +2,7 @@ from dashboard import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 admin.autodiscover()
@@ -20,6 +20,7 @@ urlpatterns = [
     
     
 ]
+urlpatterns += staticfiles_urlpatterns
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
