32d183f9697c1dc491bc702cc388a1770b849881
Adesina Joshua <tolujosh1@gmail.com>
2024-03-13 12:29:43 +0100

    extend max_length for section and migrate

--- student_classes/models.py
+++ student_classes/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 class StudentClass(models.Model):
     class_name              =   models.CharField(max_length=100, help_text='Eg- Third, Fouth,Sixth etc')
     class_name_in_numeric   =   models.IntegerField(help_text='Eg- 1,2,4,5 etc') 
-    section                 =   models.CharField(max_length=10, help_text='Eg- A,B,C etc')
+    section                 =   models.CharField(max_length=100, help_text='Eg- A,B,C etc')
     creation_date           =   models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def get_absolute_url(self):
