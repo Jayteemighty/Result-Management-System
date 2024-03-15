57fc54dbdc9ce8281de00ae97289efcdca6e3e86
Adesina Joshua <tolujosh1@gmail.com>
2024-03-15 16:09:29 +0100

    correction for subject units

--- srms/settings.py
+++ srms/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = "srms.wsgi.application"
 
 #DATABASES = {
 #    "default": {
-#        'ENGINE':'django.db.backends.mysql',
-#        'NAME':'student_management_system',
-#        'USER':'student_management_system',
-#        'PASSWORD':'new_password',
-#        'HOST':'localhost',
-#        'PORT':'3306',
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
 #    }
 #}
 
