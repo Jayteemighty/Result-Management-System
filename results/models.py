38cdcdade30c95efa8f32bd0e070f85024e23522
Adesina Joshua <tolujosh1@gmail.com>
2024-03-22 12:41:09 +0100

    feature:adding the unit feature for results

--- results/models.py
+++ results/models.py
@@ -11,7 +11,7 @@ class DeclareResult(models.Model):
     select_student = models.ForeignKey(Student, on_delete=models.CASCADE)
     marks = JSONField(blank=True)
     point = JSONField(blank=True, null=True)
-    Unit = JSONField(blank=True, null=True)
+    unit = JSONField(blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('results:declare_result')
